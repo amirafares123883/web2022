7ed9485896485c85b39ebf193964a780ae28ded2
Hp <a0595626506@gmail.com>
2022-08-04 10:16:21 +0300

    five commit

---
+++ lecture/05_loop/exa.php
@@ -0,0 +1,61 @@
+<?php
+// for ($i=1; $i <=10 ; $i++) { 
+//     if ($i<=10) {
+//         echo $i ;
+//         if ($i<10) {
+//             echo "_";
+//         }
+//     }else{
+//         echo "$i"." \n";
+//     }
+    
+// }
+
+// for ($i=1; $i <=5 ; $i++) { 
+//     for ($x=1; $x <=$i ; $x++) { 
+//         echo "*";
+//     }
+//     echo "<br/>";
+// }
+
+// for ($i=1; $i <=5 ; $i++) { 
+//     for ($x=6-$i; $x >=1 ; $x--) { 
+//         echo "*";
+//     }
+//     echo "<br/>";
+// }
+
+
+
+  
+// for ($row=0; $row <=7 ; $row++) { 
+//     for ($column=0; $column <=7 ; $column++) { 
+
+//         if ((($column == 1 or $column == 5) and $row != 0) or (($row == 0 or $row == 3) and ($column > 1 and $column < 5))){
+//             echo "*";
+//         }  else {
+//             echo " ";
+//         }   
+//         echo "\n" ;  
+//     }
+// }
+
+//Factorial
+// $n = 3;
+// $x = 1;
+// for($i=1;$i<=$n-1;$i++)
+// {
+//  $x*=($i+1); 
+// }
+// echo "The factorial of  $n = $x"."\n";
+
+
+$x = 5;
+while($x <= 15) {
+  echo "The number is:  $x <br>";
+  $x++;
+}
+?>
+
+
+
