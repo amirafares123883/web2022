adea907f1889a566ba37990b71dd7df321e5abcd
Hp <a0595626506@gmail.com>
2022-08-30 10:52:25 +0300

    commit

---
+++ lecture/09_from/sample.php
@@ -0,0 +1,25 @@
+<?php
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    /*
+  $Password = $_POST["password"];
+  $email = $_POST["email"];
+}
+if (isset($_POST['agree'])){
+    $agree=true;
+}else{
+    $agree=false;
+}
+
+*/
+}
+if(isset($_POST["password"])&&($_POST["email"])){
+    $password = ($_POST["password"]);
+    $email = ($_POST["email"]);
+    if(empty($email)&&empty($password)){
+      if(filter_var($email,FILTER_VALIDATE_EMAIL)){}
+    }
+
+  }
+?>
